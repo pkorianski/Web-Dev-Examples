66bde1adc80f095e74596ce5a3d6e20432ca0ab9
Patrick Korianski <pkorianski.engr@gmail.com>
2020-04-12 20:59:28 -0400

    Finish IT logger

---
+++ it-logger/README.md
@@ -0,0 +1,23 @@
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+## contact-finder MERN App
+
+### Purpose
+
+Review Hooks, MongoDB, Express, React, and MongoDB in a contact storing web app
+
+### Technologies Used
+
+- React
+- React-dom
+- Redux
+- React-redux
+- Redux-devtools-extension
+- Redux-thunk
+- Json-server
+- Fetch
+- Concurrently
+- Prop-types
+- Hooks
+  - useState
+  - useEffect
---
+++ it-logger/src/App.js
@@ -0,0 +1,37 @@
+import React, { useEffect, Fragment } from "react";
+import "materialize-css/dist/css/materialize.min.css";
+import M from "materialize-css/dist/js/materialize.min.js";
+import "./App.css";
+import SearchBar from "./components/layout/SearchBar";
+import Logs from "./components/logs/Logs";
+import AddBtn from "./components/layout/AddBtn";
+import AddLogModal from "./components/logs/AddLogModal";
+import EditLogModal from "./components/logs/EditLogModal";
+import AddTechModal from "./components/techs/AddTechModal";
+import TechListModal from "./components/techs/TechListModal";
+import { Provider } from "react-redux";
+import store from "./store";
+
+const App = () => {
+  useEffect(() => {
+    // Init Materilize JS
+    M.AutoInit();
+  });
+  return (
+    <Provider store={store}>
+      <Fragment>
+        <SearchBar />
+        <div className="container">
+          <AddBtn />
+          <AddLogModal />
+          <EditLogModal />
+          <AddTechModal />
+          <TechListModal />
+          <Logs />
+        </div>
+      </Fragment>
+    </Provider>
+  );
+};
+
+export default App;
