e6b3acaff3275fcf9595c4ce856f9671642663c0
Patrick Korianski <pkorianski.engr@gmail.com>
2020-05-17 19:30:01 -0400

    Finish course

---
+++ devcamper_api/routes/auth.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const {
+  register,
+  login,
+  getMe,
+  forgotPassword,
+  logout,
+} = require("../controllers/auth");
+const { protect } = require("../middleware/auth");
+
+const router = express.Router();
+
+router.post("/register", register);
+router.post("/login", login);
+router.get("/logout", logout);
+router.get("/me", protect, getMe);
+router.post("/");
+router.post("/forgotpassword", forgotPassword);
+
+module.exports = router;
---
+++ devcamper_api/routes/bootcamps.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const router = express.Router();
+const {
+  getBootcamp,
+  getBootcamps,
+  createBootcamp,
+  updateBootcamp,
+  deleteBootcamp,
+  getBootcampsInRadius,
+  bootcampPhotoUpload,
+} = require("../controllers/bootcamps");
+const { protect, authorize } = require("../middleware/auth");
+
+const Bootcamp = require("../models/Bootcamp");
+const advancedResults = require("../middleware/advancedResults");
+
+// Include other resouce routers
+const courseRouter = require("./courses");
+router.use("/:bootcampId/courses", courseRouter);
+
+router.route("/radius/:zipcode/:distance").get(getBootcampsInRadius);
+
+router
+  .route("/:id/photo")
+  .put(protect, authorize("publisher", "admin"), bootcampPhotoUpload);
+
+router
+  .route("/")
+  .get(advancedResults(Bootcamp, "courses"), getBootcamps)
+  .post(protect, authorize("publisher", "admin"), createBootcamp);
+
+router
+  .route("/:id")
+  .get(getBootcamp)
+  .put(protect, authorize("publisher", "admin"), updateBootcamp)
+  .delete(protect, authorize("publisher", "admin"), deleteBootcamp);
+
+module.exports = router;
